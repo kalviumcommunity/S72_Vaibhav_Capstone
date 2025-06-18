98538c6180697fafc81af2448bd6f903c0a7357e
VaibhavKrishna0607 <vaibhavkrishna0607@gmail.com>
2025-06-18 10:28:25 +0530

    temppush

--- Backend/routes/taskRoutes.js
+++ Backend/routes/taskRoutes.js
@@ -21,7 +21,7 @@ router.get('/:id', getTask);
 router.post('/upload-file', protect, upload.single('file'), uploadTaskFile);
 router.get('/:taskId/files/:filename', protect, downloadTaskFile);
 
-router.get('/autocomplete-titles', getTitleSuggestions);
+
 
 // Protected routes
 router.post('/', protect, createTask);
