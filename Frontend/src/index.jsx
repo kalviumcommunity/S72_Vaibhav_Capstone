b4329105b36d02c016a43437d040fcf75bfff0f8
VaibhavKrishna0607 <vaibhavkrishna0607@gmail.com>
2025-06-20 12:46:07 +0530

    frontend

--- Frontend/src/index.jsx
+++ Frontend/src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { createGlobalStyle } from 'styled-components';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
+import './pages/Pages.css';
 
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
